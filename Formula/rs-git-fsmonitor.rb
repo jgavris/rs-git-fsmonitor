0891a17c6a81d5112d9f53be8a16dec5f1e494b6
Jason Gavris <jgavris@butterflynetinc.com>
2019-01-09 18:41:07 -0500

    Release v0.1.1

--- Formula/rs-git-fsmonitor.rb
+++ Formula/rs-git-fsmonitor.rb
@@ -2,7 +2,7 @@ class RsGitFsmonitor < Formula
   desc "Git fsmonitor hook written in Rust"
   homepage "https://github.com/jgavris/rs-git-fsmonitor"
 
-  url "https://github.com/jgavris/rs-git-fsmonitor/releases/download/v0.1.0/rs-git-fsmonitor"
+  url "https://github.com/jgavris/rs-git-fsmonitor/releases/download/v0.1.1/rs-git-fsmonitor"
   sha256 "4faf1723ea75a76e0dd6187d5a3e9074fd5e93dd13f1024843d4ed256ff689e8"
 
   depends_on "watchman"
