1a3ec7859dd28f242d941c7c10e8552a590608a1
Jason Gavris <jgavris@butterflynetinc.com>
2021-12-31 11:42:13 -0500

    Ensure clean builds for release

--- bin/relase_apple.sh
+++ bin/relase_apple.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+git clean -dffx .
+
 cargo build --release --target=x86_64-apple-darwin
 cargo build --release --target=aarch64-apple-darwin
 mkdir -p target/release/apple-darwin
